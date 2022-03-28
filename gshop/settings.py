d902881232aed3ee16c5534c7f3c415a7f2dffb1
RC Regalado <ruben.alekey@gmail.com>
2022-03-28 06:55:01 -0600

    Se cambió la clave secreta

--- gshop/settings.py
+++ gshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i*8wyvhbfd+o!28etp3rli4yai^hn(3c^c=n+@o)4ezn=+5rv9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
